e40b4c5a4c56f89c26042897dccbbc0e234444f1
Kurt Weber <kdweber89@gmail.com>
2015-02-11 21:02:45 -0500

    working on styling, adding picture

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+
+
+ $('.begin').click(function() {
+    $('.begin').hide('fast');
+    /*return false;*/
+  });
+
+
+/*$('.contain').mouseover(function() {
+	$('.animation').animate({left: "50%"}, 200);
+});*/
+
+	/*	('fast');
+	return false;
+});
+
+
+
+
+$(this).	animate({
+        width: "70%"
+    }, 300 );*/
